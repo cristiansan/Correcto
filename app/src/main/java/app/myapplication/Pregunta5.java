7b109c7e7b897df2976161752dedfc34bd1be58d
home <matensegiles@gmail.com>
2015-12-01 20:40:52 -0300

    10 preguntas

---
+++ app/src/main/java/app/myapplication/Pregunta5.java
@@ -0,0 +1,43 @@
+package app.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Pregunta5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pregunta_5);
+    }
+
+    public void btnCorrect(View v)
+    {
+        ImageView correct = (ImageView)findViewById(R.id.correcto);
+        correct.setVisibility(View.VISIBLE);
+        ImageView incorrect = (ImageView)findViewById(R.id.incorrecto);
+        incorrect.setVisibility(View.INVISIBLE);
+    }
+
+    public void btnIncorrect(View v)
+    {
+        ImageView incorrect = (ImageView)findViewById(R.id.incorrecto);
+        incorrect.setVisibility(View.VISIBLE);
+        ImageView correct = (ImageView)findViewById(R.id.correcto);
+        correct.setVisibility(View.INVISIBLE);
+    }
+
+    public void btnNext(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), Pregunta6.class);
+        startActivity(intent);
+    }
+    public void btnBack(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), Pregunta4.class);
+        startActivity(intent);
+    }
+}
